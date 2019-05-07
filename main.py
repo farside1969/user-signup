12578e854cce6d3aad51f9764efefe6175c6bc0f
farside1969 <farside1969@hotmail.com>
2019-05-07 16:48:36 -0500

    progress 10%

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route("/")
+def index():
+    return render_template("base.html")
 
+@app.route("/base", methods=['POST'])
+def encrypt():
+    user = request.form['user_input']
+    return render_template('base.html', user=user)
+    
 
 if __name__ == '__main__':
 
