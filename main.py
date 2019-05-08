f171111f2d7165787cac54573b07664c49dcbcb8
farside1969 <farside1969@hotmail.com>
2019-05-08 09:01:59 -0500

    changed handelers

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, redirect, render_template
+import cgi, html, os, jinja2
+
+#template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+#jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,6 +10,10 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     return render_template("base.html")
+#@app.route("/")
+#def index():
+#    template = jinja_env.get_template('hello_form.html')
+#    return template.render()    
 
 @app.route("/base", methods=['POST'])
 def encrypt():
