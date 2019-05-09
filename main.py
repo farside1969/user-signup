2f52789f482d035de170563044f3a885a21b8113
farside1969 <farside1969@hotmail.com>
2019-05-09 15:53:23 -0500

    rearranged fields

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask, request, redirect, render_template
 import cgi, html, os, jinja2
 
+#MEGA FLOW ISSUE - REFLOW
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template('signup.html')
 
-@app.route("/base", methods=['POST'])
+@app.route("/signup", methods=['POST'])
 def sign_up():
-    user = request.form['user_input']
-    return render_template('base.html', user=user)
+    user_name = request.form['user_name']
+    return render_template('signup.html', user_name=user_name)
+
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    user_name = request.form['user_name']
+    return render_template('welcome.html', user_name=user_name)
 
 if __name__ == '__main__':
 
