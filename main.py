6be59f7e86cd05d18134138b355d7dc222fef439
farside1969 <farside1969@hotmail.com>
2019-05-10 16:44:55 -0500

    error still wrong page

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import cgi, html, os, jinja2
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+#start page
 @app.route('/')
-    return render_template("signup.html")
+def start():
+    return render_template('alt_signup.html')
 
 #form
-@app.route('/signup')
+@app.route('/alt_signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('alt_signup.html')
 
 # validation Booleans
 def empty(x):
     if x:
         return True
- 
+     
 def char_length(x):
     if len(x) > 2 and len(x) < 21:
         return True
@@ -30,7 +32,7 @@ def email_period(x):
         return True
 
 #validate form
-@app.route('/signup', methods=['POST'])
+@app.route('/alt_signup', methods=['POST'])
 def validate():
 
 #input varibles
@@ -53,44 +55,54 @@ def validate():
 #username validation
     if not empty(username):
         username_error = error_required
+        return username_error
 
     elif not char_length(username):
         username_error = "Username " + error_char_count
+        return username_error
 
     else:
         if " " in username:
             username_error = "Username " + error_no_spaces
+            return username_error
 
 #password validation
     if not empty(password):
         password_error = error_required
+        return password_error
 
     elif not char_length(password):
         password_error = "Password " + error_char_count
+        return password_error
 
     else:
         if " " in password:
             password_error = "Password " + error_no_spaces
+            return password_error
 
 #verify password validation
     if password_verify != password:
         password_verify_error = "Passwords do not match"
-        password_error = "Passwords do not match"
+        return password_verify_error
 
 #e-mail validation
     if empty(email):
         if not char_length(email):
             email_error = "Email " + error_char_count
+            return email_error
  
         elif not email_symbol(email):
             email_error = "Email must contain one @ symbol"
+            return email_error
 
         elif not email_period(email):
             email_error = "Email must contain one . (dot)"
+            return email_error
 
         else:
             if " " in email:
                 email_error = "Email " + error_no_spaces
+                return email_error
 
 #no error redirects to welcome
 #error will display error message
@@ -98,7 +110,7 @@ def validate():
         username = username
         return redirect('/welcome?username={0}'.format(username))
     else:
-        return render_template('signup.html', username_error=username_error, username=username, password_error=password_error, password=password, password_verify_error=password_verify_error, password_verify=password_verify, email_error=email_error, email=email)
+        return render_template('alt_signup.html', username_error=username_error, username=username, password_error=password_error, password=password, password_verify_error=password_verify_error, password_verify=password_verify, email_error=email_error, email=email)
 
 #redirect to welcome
 @app.route('/welcome')
@@ -107,5 +119,4 @@ def valid_signup():
     return render_template('welcome.html', username=username)
 
 if __name__ == '__main__':
-
     app.run()
