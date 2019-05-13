a2fe28a5ae5b0a96b5c1643787c8be9fa7b7fd1c
farside1969 <farside1969@hotmail.com>
2019-05-13 09:18:47 -0500

    adjusted page tabs and main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ app.config['DEBUG'] = True
 #start page
 @app.route('/')
 def start():
-    return render_template('alt_signup.html')
+    return render_template('signup.html')
 
 #form
-@app.route('/alt_signup')
+@app.route('/signup')
 def signup():
-    return render_template('alt_signup.html')
+    return render_template('signup.html')
 
 # validation Booleans
 def empty(x):
     if x:
         return True
-     
+       
 def char_length(x):
     if len(x) > 2 and len(x) < 21:
         return True
@@ -30,9 +30,9 @@ def email_symbol(x):
 def email_period(x):
     if x.count('.') == 1:
         return True
-
+        
 #validate form
-@app.route('/alt_signup', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def validate():
 
 #input varibles
@@ -55,54 +55,43 @@ def validate():
 #username validation
     if not empty(username):
         username_error = "Username " + error_required
-        return username_error
 
     elif not char_length(username):
         username_error = "Username " + error_char_count
-        return username_error
 
     else:
         if " " in username:
             username_error = "Username " + error_no_spaces
-            return username_error
 
 #password validation
     if not empty(password):
         password_error = "Password " + error_required
-        return password_error
 
     elif not char_length(password):
         password_error = "Password " + error_char_count
-        return password_error
 
     else:
         if " " in password:
             password_error = "Password " + error_no_spaces
-            return password_error
 
 #verify password validation
     if password_verify != password:
         password_verify_error = "Passwords do not match"
-        return password_verify_error
-
+        
 #e-mail validation
     if empty(email):
         if not char_length(email):
             email_error = "Email " + error_char_count
-            return email_error
  
         elif not email_symbol(email):
             email_error = "Email must contain one @ symbol"
-            return email_error
 
         elif not email_period(email):
             email_error = "Email must contain one . (dot)"
-            return email_error
 
         else:
             if " " in email:
                 email_error = "Email " + error_no_spaces
-                return email_error
 
 #no error redirects to welcome
 #error will display error message
@@ -110,7 +99,7 @@ def validate():
         username = username
         return redirect('/welcome?username={0}'.format(username))
     else:
-        return render_template('/alt_signup.html', username_error=username_error, username=username, password_error=password_error, password=password, password_verify_error=password_verify_error, password_verify=password_verify, email_error=email_error, email=email)
+        return render_template('/signup.html', username_error=username_error, username=username, password_error=password_error, password=password, password_verify_error=password_verify_error, password_verify=password_verify, email_error=email_error, email=email)
 
 #redirect to welcome
 @app.route('/welcome')
